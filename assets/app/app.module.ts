e6a3495f635366e4650796d569c019a75a1164a7
= <karfannacif@gmail.com>
2017-06-06 22:19:01 -0300

    class 93 ok

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 
 //ROUTING
 import { routing } from './app.routing';
 
-// COMPONENTS
+//MODULE
+import { MessageModule } from './messages/message.module';
 
+// COMPONENTS
 import { AppComponent } from "./app.component";
 import { AuthenticationComponent } from './auth/authentication.component';
 import { ErrorComponent } from './errors/error.component';
 import { HeaderComponent } from './header.component';
-import { LogoutComponent } from './auth/logout.component';
-import { MessageComponent } from './messages/message.component';
-import { MessageListComponent } from './messages/message-list.component';
-import { MessageInputComponent } from './messages/message-input.component';
-import { MessagesComponent } from './messages/messages.component';
-import { SigninComponent } from './auth/signin.component';
-import { SignupComponent } from './auth/signup.component';
+
 
 // SERVICES
 import { AuthService } from './auth/auth.service';
@@ -28,23 +23,15 @@ import { MessageService } from './messages/message.service';
 @NgModule({
     declarations: [
         AppComponent,
-        MessageComponent,
-        MessageListComponent,
-        MessageInputComponent,
-        MessagesComponent,
         AuthenticationComponent,
         HeaderComponent,
-        LogoutComponent,
-        SigninComponent,
-        SignupComponent,
         ErrorComponent
     ],
     imports: [
         BrowserModule,
-        FormsModule,
         routing,
-        ReactiveFormsModule,
-        HttpModule
+        HttpModule,
+        MessageModule
     ],
     providers: [
         MessageService,
